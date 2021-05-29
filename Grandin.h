20b545cdbeb496a4a5bbe2ce30c849ce3356cd76
ysard <ysard@users.noreply.github.com>
2021-05-29 05:09:08 +0200

    Add mappings for Grandin(Toshiba) and LG remotes

---
+++ Grandin.h
@@ -0,0 +1,48 @@
+
+#define INFO 0x2FD6897 // info
+#define MUTE 0x2FD08F7 // mute > mute
+#define ONE 0x2FD807F // 1
+#define TWO 0x2FD40BF // 2
+#define THREE 0x2FDC03F // 3
+#define FOUR 0x2FD20DF // 4
+#define FIVE 0x2FDA05F // 5
+#define SIX 0x2FD609F // 6
+#define SEVEN 0x2FDE01F // 7
+#define EIGHT 0x2FD10EF // 8
+#define NINE 0x2FD906F // 9
+#define ZERO 0x2FD00FF // 0
+//#define SOURCE 0x2FD28D7 // source
+#define NEXT_LANGUAGE 0x2FD4AB5 // audio > AudioNextLanguage
+//#define MENU 0x2FDDA25 // menu
+#define ZOOM 0x2FDA25D // screen size > Zoom/aspect ratio
+//#define LOOP 0x2FD02FD // loop
+#define SELECT 0x2FD50AF // enter > select
+#define SKIP_NEXT 0x2FDD827 // ch + > page up (skip to next)
+#define SKIP_PREVIOUS 0x2FDF807 // ch - > page down (skip to previous)
+//#define LIST 0x2FD18E7 // list
+//#define SLEEP 0x2FDB04F // sleep
+//#define EPG 0x2FD926D // EPG
+//#define FAVORITES 0x2FD42BD // favorites
+#define REWIND 0x2FD12ED // red (rewind) > rewind
+#define FAST_FORWARD 0x2FDF20D // green (fast forward) > fast forward
+#define JUMP_BACKWARD 0x2FD0AF5 // yellow (previous) > small seek step backwards 7 seconds //ù
+#define JUMP_FORWARD 0x2FDD22D // blue (next) > jump forward 10 seconds.
+#define PAUSE_PLAY 0x2FDE817 // ? (play/pause) > pause/play
+#define STOP 0x2FDAA55 // still (stop) > stop
+#define NEXT_SUBTITLE 0x2FD2AD5 // subtitle > next subtitle
+//#define RADIO 0x2FDCA35 // radio
+//#define 0x2FDEA15 // ?
+//#define 0x2FDBA45 // dmp
+#define MENU 0x2FD7A85 // index > contextual menu
+#define SCREENSHOT 0x2FD6A95 // record > screenshot
+
+
+// Repeatable keys
+
+#define BACK 0x2FD22DD // exit > back
+#define UP 0x2FD9867 // up
+#define DOWN 0x2FDB847 // down
+#define LEFT 0x2FD629D // left > left (seek backward)
+#define RIGHT 0x2FDE21D // right > right (seek forward)
+#define VOL_UP 0x2FD58A7 // vol + > vol up
+#define VOL_DOWN 0x2FD7887 // vol - > vol down
---
+++ LG.h
@@ -0,0 +1,53 @@
+
+#define INFO 0x20DF55AA // info
+#define MUTE 0x20DF906F // mute > mute
+#define ONE 0x20DF8877 // 1
+#define TWO 0x20DF48B7 // 2
+#define THREE 0x20DFC837 // 3
+#define FOUR 0x20DF28D7 // 4
+#define FIVE 0x20DFA857 // 5
+#define SIX 0x20DF6897 // 6
+#define SEVEN 0x20DFE817 // 7
+#define EIGHT 0x20DF18E7 // 8
+#define NINE 0x20DF9867 // 9
+#define ZERO 0x20DF08F7 // 0
+//#define SOURCE 0x20DFD02F // source
+#define NEXT_LANGUAGE 0x20DF8976 // audio > AudioNextLanguage
+#define ZOOM 0x20DF9E61 // screen size > Zoom/aspect ratio
+//#define LOOP 0x2FD02FD // loop
+#define SELECT 0x20DF22DD // enter > select
+#define SKIP_NEXT 0x20DF00FF // ch + > page up (skip to next)
+#define SKIP_PREVIOUS 0x20DF807F // ch - > page down (skip to previous)
+//#define LIST 0x20DFCA35 // list
+//#define SLEEP 0x20DF10EF // sleep
+//#define FAVORITES 0x20DF7887 // favorites
+// 0x20DFD52A // guide
+// 0x20DF04FB // TEXT
+// 0x20DF847B // T.OPT
+// 0x20DFC23D // settings
+// 0x20DF14EB // back
+// 0x20DF0CF3 // AV MODE
+#define REWIND 0x20DFF10E // rewind > rewind
+#define FAST_FORWARD 0x20DF718E // fast forward > fast forward
+// 0x20DF4EB1 // red
+// 0x20DF8E71 // green
+#define JUMP_BACKWARD 0x20DFC639 // yellow (previous) > small seek step backwards 7 seconds //ù
+#define JUMP_FORWARD 0x20DF8679 // blue (next) > jump forward 10 seconds.
+#define PAUSE_PLAY 0x20DF0DF2 // play, 20DF5DA2 (pause) > pause/play
+#define STOP 0x20DF8D72 // still (stop) > stop
+#define NEXT_SUBTITLE 0x20DF9C63 // subtitle > next subtitle
+//#define RADIO 0x20DF0FF0 // radio
+//#define MENU 0x20DFA25D // quick menu > contextual menu
+#define MENU 0x20DF4EB1 // red > quick menu
+#define SCREENSHOT 0x20DFBD42 // record > screenshot
+
+
+// Repeatable keys
+
+#define BACK 0x20DFDA25 // exit > back
+#define UP 0x20DF02FD // up
+#define DOWN 0x20DF827D // down
+#define LEFT 0x20DFE01F // left > left (seek backward)
+#define RIGHT 0x20DF609F // right > right (seek forward)
+#define VOL_UP 0x20DF40BF // vol + > vol up
+#define VOL_DOWN 0x20DFC03F // vol - > vol down
