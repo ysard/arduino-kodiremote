faa1f8e0be8c3714c835248f81fe01aa6491ca97
ysard <ysard@users.noreply.github.com>
2022-09-22 13:53:39 +0200

    Add licenses on headers

--- Grandin.h
+++ Grandin.h
@@ -1,4 +1,21 @@
-
+/**
+ * arduino-kodiremote is an USB Infrared Remote receiver for Kodi
+ * using an Arduino Pro Micro (Leonardo).
+ *
+ * Copyright (C) 2019-2022 Ysard <ysard@users.noreply.github.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #define INFO 0x2FD6897 // info
 #define MUTE 0x2FD08F7 // mute > mute
 #define ONE 0x2FD807F // 1
